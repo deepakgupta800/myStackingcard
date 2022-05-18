cb4a7adb2d2731fe2238238a65b082b83c76c7a7
DeepakGupta800 <deepakgupta8006462597@gmail.com>
2022-05-18 13:08:37 +0530

    my stackingcard

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./index.css";
+
+ReactDOM.render( < >
+    <
+    h1 className = 'heading' > Stacking cards < /h1> < p className = "text-md" > 👇Scroll down to see the effect. </p > < div className = 'dit' >
+    <
+    button className = "btn" > Article & Download < /button> </div > < div className = 'cards' >
+    <
+    ul className = 'card' >
+    <
+    li className = 'card1' >
+    <
+    div className = 'pic' >
+    <
+    h1 > card one < /h1> <p > my name is deepak gupta </p > < button className = "bn" > read more < /button></div >
+    <
+    img src = "https://picsum.photos/200/300"
+    alt = "" / >
+
+    <
+    /li> <li className = 'card1' > <
+    div className = 'pic' >
+    <
+    h1 > card two < /h1> <p > my name is deepak gupta </p > < button className = "bn" > read more < /button></div >
+    <
+    img src = "https://picsum.photos/200/300"
+    alt = "" / >
+
+    <
+    /li> <li className = 'card1' > <
+    div className = 'pic' >
+    <
+    h1 > card three < /h1> <p > my name is deepak gupta </p >
+    <
+    button className = "bn" > read more < /button></div >
+    <
+    img src = "https://picsum.photos/200/300"
+    alt = "" / >
+
+    <
+    /li> </ul > < /div > <
+    div className = 'para' >
+
+
+    <
+    p className = 'para' > Lorem ipsum dolor sit amet consectetur, adipisicing elit. < br / >
+    Rem repellat non ratione eum provident quaerat obcaecati < br / >
+    dicta autem voluptates sit cum quis distinctio, atque sint < br / >
+    commodi reprehenderit perspiciatis iure velit molestiae eos < br / >
+    molestias ipsa nihil quia ? Porro tempore in fugit, aspernatur < br / >
+    et temporibus aliquam magnam adipisci deleniti dicta repellat < br / >
+    alias nostrum impedit quidem odit excepturi nam. < br / >
+    Itaque numquam unde quae dignissimos exercitationem placeat, < br / >
+    ipsa amet tempora vitae ullam animi!Hic asperiores in esse atque < br / >
+    expedita dolorem officia illo commodi fugit earum ab, assumenda < br / >
+    optio, sed repellat.Maiores laudantium reprehenderit suscipit < br / >
+    accusamus assumenda, debitis nemo ut sapiente numquam doloremque < br / >
+    alias explicabo minus autem harum illum dicta.Unde obcaecati inventore < br / >
+    repellat id eligendi eaque, rerum aliquam maiores aspernatur cumque < br / >
+    tenetur quo qui mollitia atque quasi voluptate ipsum neque explicabo < br / >
+    voluptatem quibusdam beatae quae iste vero doloribus!Natus libero < br / > < br / >
+    quibusdam molestias harum atque, assumenda doloribus quis, quam corporis, < br / >
+    voluptas suscipit magni!Enim sapiente totam consequatur repellat maxime < br / >
+    placeat perferendis iste maiores soluta, obcaecati incidunt expedita eius < br / >
+    cumque ipsa quo sed.Minus distinctio culpa cumque labore nulla laborum < br / >
+    ex, explicabo magni mollitia eaque aliquam vero blanditiis quae, id < br / >
+    necessitatibus error voluptas harum tempora sint porro.Facilis beatae < br / >
+    quasi soluta et ipsum suscipit repellendus inventore. < /p>
+
+
+
+    <
+    /div> </ > ,
+    document.getElementById("root"));
